26b1617645fbb92d0ce45fad48bc02ea4aed9582
ru14 <74753345+ru14@users.noreply.github.com>
2021-05-25 12:39:28 -0700

    basic handlebar created

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-router.get("/", (req, res)=> {
-    res.send("hello")
-})
+// router.get("/", (req, res)=> {
+//     res.send("hello")
+// })
 router.get('/', async (req, res) => {
     
     res.render('homepage');
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const path = require('path');
 //const helpers = require('./utils/helpers');
 
 
@@ -32,7 +33,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
