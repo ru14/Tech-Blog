df9e3a75356e9209862a9adc2bb32dbeea9860ec
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-02 13:45:16 -0700

    index made

--- models/index.js
+++ models/index.js
@@ -1,13 +1,37 @@
 const User = require('./User');
-//const Project = require('./Project');
+const Comment = require('./comment');
+const Blog = require('./blog');
 
-//User.hasMany(Project, {
- // foreignKey: 'user_id',
- // onDelete: 'CASCADE'
-//});
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+});
 
-//Project.belongsTo(User, {
-//  foreignKey: 'user_id'
-//});
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+
 
 module.exports = { User };
