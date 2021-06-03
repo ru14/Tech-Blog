819d69f6d126225822a6c664e80de0c869a0aece
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-02 17:02:43 -0700

    routes some more

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
-
+const { blog, User, comment } = require('../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 //get all blogs
 router.get('/', async (req, res) => {
-  res.render('post',{ postblogs: postblogs });// name of handlebar view
+  res.render('blog',{ blogs: blogs });// name of handlebar view
 });
 
 //get one blog
 router.get('/:num', async (req, res) => {
-  return res.render('post', postblogs[req.params.num - 1]);
+  return res.render('blog', blogs[req.params.num - 1]);
 });
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 
-
+router.use('/blog', blogRoutes);
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 
 
