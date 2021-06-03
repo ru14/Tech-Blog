ce3b6f9d8a94b611e86d859bccc4d656d791bd58
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-02 17:41:09 -0700

    get all

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -5,7 +5,44 @@ const withAuth = require('../../utils/auth');
 
 //get all blogs
 router.get('/', async (req, res) => {
-  res.render('blog',{ blogs: blogs });// name of handlebar view
+  blog.findAll({
+    attributes: [
+      'id',
+      'bloger_name',
+      'blog_header',
+      'description',
+      'cerated_at'
+    ],
+    order: [
+      ['created_at',
+        'DESC']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['name']
+      },
+      {
+        model: Comment,
+        attributes: [
+          'id',
+          'comment_text',
+          'blog_id',
+          'user_id',
+          'created_at'],
+        include: {
+          model: User,
+          attributes: ['name']
+        }
+      }
+    ]
+  })
+  .then(dbPostData => res.json(dbPostData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+// res.render('blog',{ blogs: blogs });// name of handlebar view
 });
 
 //get one blog
