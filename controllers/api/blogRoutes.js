ee291f0b9d4c48a742fdf685dcad9db8521c50c0
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-04 13:32:38 -0700

    update

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -85,21 +85,41 @@ router.get('/', async (req, res) => {
   // res.render('blog',{ blogs: blogs });// name of handlebar view
 });
 //update--put
-//post create require 
-router.post('/:id', withAuth, (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Post.update({
-    blog_header: req.body.title,
-    description: req.body.content
+    blog_header: req.body.blog_header,
+    description: req.body.description,
   }, {
     where: {
       id: req.params.id
     }
   }).then(dbblogData => {
-    if (!dbPostData) {
+    if (!dbblogData) {
       res.status(404).json({ message: 'No post found with this id' });
       return;
     }
-    res.json(dbPostData);
+    res.json(dbblogData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+
+router.post('/:id', withAuth, (req, res) => {
+  Post.create({
+    bloger_name: req.session.bloger_name,
+    blog_header: req.body.title,
+    description: req.body.content,
+    user_id: req.session.user_id
+  }).then(dbblogData => {
+    if (!dbblogData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbblogData);
   })
     .catch(err => {
       console.log(err);
