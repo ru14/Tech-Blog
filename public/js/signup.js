63f0881c2ec4dbfc1694686ae11567478504c235
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-09 14:38:01 -0700

    err fixed

--- public/js/signup.js
+++ public/js/signup.js
@@ -13,7 +13,8 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        console.log("sucessfull ")
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
