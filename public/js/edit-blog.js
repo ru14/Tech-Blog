4ffb04ac22661fc219988540e2d6dbc03373a706
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-09 14:29:59 -0700

    error fixed

--- public/js/edit-blog.js
+++ public/js/edit-blog.js
@@ -1,8 +1,8 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
+    const title = document.querySelector('input[name="blog_header"]').value.trim();
+    const content = document.querySelector('input[name="discription"]').value.trim();
     console.log(title);
     console.log(content);
 
