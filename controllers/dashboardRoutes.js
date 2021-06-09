9c9b8386ff3edf9d556dace10404e4411b649016
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-09 14:01:44 -0700

    route made

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -13,7 +13,6 @@ router.get('/', async (req, res) => {
         },
         attributes: [
             'id',
-            'bloger_name',
             'blog_header',
             'description',
             'created_at'
@@ -39,8 +38,8 @@ router.get('/', async (req, res) => {
         ]
     })
         .then(dbblogData => {
-            const blogs = dbblogData.map(blog => blog.get({ plain: true}));
-            res.render('dashbord', {blogs, loggedIn: true});
+            const blogs = dbblogData.map(blog => blog.get({ plain: true }));
+            res.render('dashbord', { blogs, loggedIn: true });
         })
         .catch(err => {
             console.log(err);
@@ -51,3 +50,50 @@ router.get('/', async (req, res) => {
 
 //edit blog route
 // new blog route
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'blog_header',
+            'description',
+            'created_at'
+        ],
+        include: [{
+            model: User,
+            attributes: ['name']
+        },
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                    'comment_text',
+                    'blog_id',
+                    'user_id',
+                    'created_at'],
+            include: {
+                model: User,
+                attributes: ['name']
+            }
+        }
+        ]
+    })
+        .then(dbblogData => {
+            if (!dbblogData) {
+                res.status(404).json({ message: 'No blog found with this id' });
+                return;
+            }
+
+            const blog = dbblogData.get({ plain: true });
+            res.render('edit-blog', { blog, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+router.get('/new', (req, res) => {
+    res.render('new-blog');
+});
